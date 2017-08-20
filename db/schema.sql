4187b0d8578f86bd5c4d6758fb6ad22a6c47493e
Seana Curran <shaunacurran@Shaunas-MacBook-Pro.local>
2017-08-20 17:30:03 -0500

    fixed db

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,11 @@ CREATE DATABASE burgers_db;
 
 use burgers_db;
 
-INSERT INTO burgers (burger_name, devoured, date) VALUES 
+CREATE TABLE burgers (
+	id INTEGER auto_increment,
+    burger_name VARCHAR (255) NOT NULL, 
+    devoured BOOLEAN NOT NULL,
+    date TIMESTAMP NOT NULL,
+    PRIMARY KEY (ID)
 
-('SPICY BUFFALO BURGER', TRUE, '2012-01-28 00:03:45'),
-('BBQ PINEAPPLE BURGER', TRUE, '2013-02-14 00:02:45'),
-('SPICY BUFFALO BURGER', TRUE, '2014-12-11 00:01:10');
-
+);
