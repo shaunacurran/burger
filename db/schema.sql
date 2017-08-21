248f15f06fcf029776b1c8834ec5dd4b7db1ee6f
Seana Curran <shaunacurran@Shaunas-MacBook-Pro.local>
2017-08-20 21:32:38 -0500

    fixed boolean default

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ use burgers_db;
 CREATE TABLE burgers (
 	id INTEGER auto_increment,
     burger_name VARCHAR (255) NOT NULL, 
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     date TIMESTAMP NOT NULL,
     PRIMARY KEY (ID)
 
